e1bdba7557fa0772737eef6d1a94d39be958b12f
andip71 <andreasp@gmx.de>
2015-04-15 14:20:21 +0200

    printk: Add sys kernel interface to configure linux printk logging
    - now logging can be switched on and off via /sys/kernel/printk_mode interface
    - default is 1 (on)
    - Enabled time stamp information in logs again

--- compile-script.sh
+++ compile-script.sh
@@ -2,7 +2,7 @@ export ARCH=arm && export SUBARCH=arm
 
 make clean && make mrproper
 
-export CROSS_COMPILE=/home/andrea/U4/bin/arm-eabi-
+export CROSS_COMPILE=/home/andrea/UberTC-4.9/bin/arm-eabi-
 
 make tegra_s8515_android_defconfig
 
