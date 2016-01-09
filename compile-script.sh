c070495aaa534bdd6e6fbfaf53d024c0c77f7aa5
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-09 02:16:58 +0000

    ksm: check and skip page, if it is already scanned
    On system like Android where most of the process are forked
    from parent w/o execve, KSM can scan same page multiple times
    in one scan cycle. There is no advantage in scanning same page
    multiple times for merging. During testing with Android, it was
    observed around 60% pages are skipped for each scan cycle.
    
    Change-Id: I0cf01802f0b4d61fcab92558beb9e1c660dc9a77
    Link: http://lkml.kernel.org/r/CAMrOTPgBtANS_ryRjan0-dTL97U7eRvtf3dCsss=Kn+Uk89fuA@mail.gmail.com
    Signed-off-by: Pradeep Sawlani <sawlani@amazon.com>
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- compile-script.sh
+++ compile-script.sh
@@ -1,6 +1,6 @@
 export ARCH=arm && export SUBARCH=arm
 
-export CROSS_COMPILE=/home/andrea/UxL4.8/bin/arm-eabi-
+export CROSS_COMPILE=/home/andrea/U4.8/bin/arm-eabi-
 
 make tegra_s8515_android_defconfig
 
