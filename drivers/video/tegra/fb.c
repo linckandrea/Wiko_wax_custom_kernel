eeaad2f4eaf63c6e62a7b41266e9722f6a88a5bc
linckandrea <linckandrea01@gmail.com>
2018-09-04 02:11:03 +0200

    Some code optmizations(part1+2+3)

--- drivers/video/tegra/fb.c
+++ drivers/video/tegra/fb.c
@@ -160,14 +160,8 @@ static int tegra_fb_set_par(struct fb_info *info)
 
 		memcpy(&tegra_fb->mode, &m, sizeof(tegra_fb->mode));
 
-
-#ifdef CONFIG_MACH_S8515
-		info->mode = (struct fb_videomode *)
-			fb_find_nearest_mode(&m, &info->modelist);
-#else
-//Ivan		info->mode = (struct fb_videomode *)&tegra_fb->mode;
 		info->mode = (struct fb_videomode *)fb_find_nearest_mode(&m, &info->modelist);
-#endif   
+
 		if (!info->mode) {
 			dev_warn(&tegra_fb->ndev->dev, "can't match video mode\n");
 			info->mode = old_mode;
